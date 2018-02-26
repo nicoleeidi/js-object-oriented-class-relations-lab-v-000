0b7c059bdeec75671f0d15b659d493e3772e39ef
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-26 21:59:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ class Driver {
   passengers(){
     let newtrips= store.trips.filter(trip => {return trip.driverId===this.id})
     let passengerids= newtrips.map(function(trip){return trip.passengerId})
-    //return passengerids.map(function(id){return store.passengers.id===id}) //cannot reference store within map?
+    return passengerids.map(function(id){return store.passengers.id===id}) //cannot reference store within map?
     //return store.passengers.filter(passenger => {return passenger.id===passengerids})
-    return store.passengers.filter(function(passenger){
-      if (passengerids.includes(passenger.id)){return passenger}
-    })
+    // return store.passengers.filter(function(passenger){
+    //   if (passengerids.includes(passenger.id)){return passenger}
+    // })
   }
 }
 let passenger1Id=0
